1a254ab31d860dd13e59c2618070c34d5676a638
rickjp <3741326+RickJP@users.noreply.github.com>
2020-05-12 22:26:56 +0900

    Forgot to exclide the build from gitignore so I could upload it

---
+++ client/build/precache-manifest.0c423435a6b12cfc1c81b5f137a8e6b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed8ab0cf45309faeedeb24e3dda108c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "147a9a72637292699470",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "4e9518f5d1f56065e5d2",
+    "url": "/static/css/main.947f2138.chunk.css"
+  },
+  {
+    "revision": "147a9a72637292699470",
+    "url": "/static/js/2.52c2ceaf.chunk.js"
+  },
+  {
+    "revision": "4e9518f5d1f56065e5d2",
+    "url": "/static/js/main.8e93b8fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0e4c6b31feadee72442de6770cb9ed67",
+    "url": "/static/media/Logo.0e4c6b31.png"
+  }
+]);
